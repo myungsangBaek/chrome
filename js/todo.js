190671a0ba64b25e591da699d08bcb0077794ea0
myungsangBaek <bms6168@naver.com>
2022-06-07 06:30:51 +0900

    add : Deleting todos

--- js/todo.js
+++ js/todo.js
@@ -2,14 +2,19 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const deleteToDo = (event) => {
+  const li = event.target.parentElement;
+  li.remove();
+};
 const paintToDo = (newTodo) => {
   const li = document.createElement("li");
   const span = document.createElement("span");
+  span.innerText = newTodo;
   const button = document.createElement("button");
   button.innerText = "X";
+  button.addEventListener("click", deleteToDo);
   li.appendChild(span);
   li.appendChild(button);
-  span.innerText = newTodo;
   toDoList.appendChild(li);
 };
 
